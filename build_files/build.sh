36fef327c27979e9971db3152e6be89eef1976c4
( ) MEGA [ie: Jost Grant] <megalisp@proton.me>
2025-08-05 19:25:14 -0500

    Bruh...

--- build_files/build.sh
+++ build_files/build.sh
@@ -6,11 +6,11 @@ set -ouex pipefail && \
   dnf5 install -y \
     git dnf5-plugins \
     chibi-scheme chez-scheme racket raylib \
-    papirus-icon-theme google-noto-emoji-fonts google-nono-fonts-all dejavu-sans-fonts \
+    papirus-icon-theme google-noto-emoji-fonts dejavu-sans-fonts \
     google-roboto-fonts google-roboto-mono-fonts google-roboto-slab-fonts \
     gnome-keyring thunar tumbler feh rofi \
-    obs-studio chatterino2 vlc v4l-utils v4l2loopback  cef chromium \
-    pavucontrol playerctl mpv mpd sox ffmpeg yt-dlp python3-streamlink \
+    obs-studio chatterino2 vlc cef chromium \
+    pavucontrol playerctl mpv sox ffmpeg yt-dlp python3-streamlink \
     qemu-kvm x11vnc input-leap kde-connect \
     picom rofi \ 
     --skip-unavailable
