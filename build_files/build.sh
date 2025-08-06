eada8243e6b528013f887199a8951f67c7bf172c
( ) MEGA [ie: Jost Grant] <source.megalisp@proton.me>
2025-08-05 19:18:28 -0500

    Comment caused error? Also remove all extra newlines for now to simplify.

--- build_files/build.sh
+++ build_files/build.sh
@@ -5,19 +5,12 @@ set -ouex pipefail
 set -ouex pipefail && \
   dnf5 install -y \
     git dnf5-plugins \
-\
     chibi-scheme chez-scheme racket raylib \
-\ 
     papirus-icon-theme google-noto-emoji-fonts google-nono-fonts-all dejavu-sans-fonts \
     google-roboto-fonts google-roboto-mono-fonts google-roboto-slab-fonts \
-\
     gnome-keyring thunar tumbler feh rofi \
-\
     obs-studio chatterino2 vlc v4l-utils v4l2loopback  cef chromium \
-\
     pavucontrol playerctl mpv mpd sox ffmpeg yt-dlp python3-streamlink \
-\
     qemu-kvm x11vnc input-leap kde-connect \
-\
-    picom rofi \ #move to s-top? \
+    picom rofi \ 
     --skip-unavailable
